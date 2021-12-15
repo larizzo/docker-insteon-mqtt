e02474530fb57b29f3a4fe4f173bb4d67b873a39
larizzo <shawn@rizzoli.ca>
2021-12-15 20:07:26 +0000

    fixed location of default config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 git clone 'https://github.com/TD22057/insteon-mqtt.git' /opt/insteon-mqtt && \
-pip install /opt/insteon-mqtt \
-
-/bin/cp /opt/insteon-mqtt/config.yaml /config/insteon_mqtt.yaml.default
+pip install /opt/insteon-mqtt && \
+/bin/cp /opt/insteon-mqtt/config-example.yaml /config/insteon_mqtt.yaml.default
 
   if [ ! -f /config/insteon_mqtt.yaml ]; then
     echo "Copying insteon_mqtt.yaml"
-    /bin/cp /opt/insteon-mqtt/config.yaml /config/insteon_mqtt.yaml
+    /bin/cp /config/insteon_mqtt.yaml.default /config/insteon_mqtt.yaml
   fi
 
 insteon-mqtt /config/insteon_mqtt.yaml start
